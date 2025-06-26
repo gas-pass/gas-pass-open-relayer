a2ea521943cea3e72ad9f206b4db0495034a994d
mvines <m@vin.es>
2025-06-26 14:58:02 -0300

    Removing comment
    These tools are part of the project environment setup

--- src/program-rust/do.sh
+++ src/program-rust/do.sh
@@ -55,7 +55,6 @@ perform_action() {
         ;;
     dump)
         # Dump depends on tools that are not installed by default and must be installed manually
-        # TODO: Please add the related pre-install scripts for env
         # - greadelf
         # - rustfilt
         (
